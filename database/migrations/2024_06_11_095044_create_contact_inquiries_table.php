669697503f5e3676cbd59ef14c5c121fb18aad6b
Njokimary <marynjoki738@gmail.com>
2024-06-11 12:53:58 +0300

    run the contact inquiry migration

--- database/migrations/2024_06_11_095044_create_contact_inquiries_table.php
+++ database/migrations/2024_06_11_095044_create_contact_inquiries_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('contact_inquiries', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->text('message');
             $table->timestamps();
+            
         });
     }
 
